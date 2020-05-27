ae2aa8ea28b28956c4de47f107d30b113e84111d
Райхан Кульмагамбетова <raihankulmagambetova@iMac-Rajhan.local>
2020-05-27 11:36:21 +0400

    remove name

--- js/banner.js
+++ js/banner.js
@@ -2,70 +2,70 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     let tabContentWindow = document.querySelector('.container')
-    let tabContent = document.querySelectorAll('.tabcontent');
-    let tabCheck = document.querySelectorAll('.point');
+    let tabsContent = document.querySelectorAll('.tabscontent');
+    let tabsCheck = document.querySelectorAll('.point');
     let tabCheckParent = document.querySelector('.check-points');
-    let pointer = document.querySelectorAll('.button-back');
+    let arrow = document.querySelectorAll('.arrow-back');
     let tabField = document.querySelector('.content-lenght');
     let width = window.getComputedStyle(tabContentWindow).width
 
-    tabField.style.width = 100 * tabContent.length + '%';
+    tabField.style.width = 100 * tabsContent.length + '%';
     tabField.style.display = 'flex';
     tabField.style.transition = '0.5s all';
 
     tabContentWindow.style.overflow = 'hidden';
 
-    tabContent.forEach(item => {
+    tabsContent.forEach(item => {
         item.style.width = width;
     });
 
     let offset = 0;
-    let activePointerIndex = 0;
+    let activeArrowIndex = 0;
 
-    pointer[1].addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (tabContent.length - 1)) {
+    arrow[1].addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (tabsContent.length - 1)) {
             offset = 0;
-            activePointerIndex = 0;
+            activeArrowIndex = 0;
         } else {
             offset += +width.slice(0, width.length - 2); 
-            activePointerIndex += 1; 
+            activeArrowIndex += 1; 
         }
 
         tabField.style.transform = `translateX(-${offset}px)`;
-        setActivePointer(activePointerIndex);
+        setActiveArrow(activeArrowIndex);
     });
 
-    pointer[0].addEventListener('click', () => {
+    arrow[0].addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (tabContent.length - 1);
-            activePointerIndex = tabField.childElementCount - 1;
+            offset = +width.slice(0, width.length - 2) * (tabsContent.length - 1);
+            activeArrowIndex = tabField.childElementCount - 1;
         } else {
             offset -= +width.slice(0, width.length - 2);  
-            activePointerIndex -= 1;
+            activeArrowIndex -= 1;
         }
 
         tabField.style.transform = `translateX(-${offset}px)`;
-        setActivePointer(activePointerIndex);
+        setActiveArrow(activeArrowIndex);
     });
 
     tabCheckParent.addEventListener('click', (e) => {
         let target = e.target;
         
-        tabCheck.forEach((item, i) => {
+        tabsCheck.forEach((item, i) => {
             if (target == item) {
-                activePointerIndex = i;
-                setActivePointer(activePointerIndex);
+                activeArrowIndex = i;
+                setActiveArrow(activeArrowIndex);
                 offset = +width.slice(0, width.length - 2) * i
             }
         });
         tabField.style.transform = `translateX(-${offset}px)`;
     });
 
-    function setActivePointer(i) {
-        tabCheck.forEach(item => {
+    function setActiveArrow(i) {
+        tabsCheck.forEach(item => {
             item.classList.remove("active-point");
         });
-        tabCheck[i].classList.add("active-point");
+        tabsCheck[i].classList.add("active-point");
     }
 });
 
